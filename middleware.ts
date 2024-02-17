e2f68b00467b8befe244c45a234c7457b426a9e5
Sohom <sohommondalofficial@gmail.com>
2024-02-17 16:25:58 +0530

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/anyone-can-visit-this-route"],
-  ignoredRoutes: ["/no-auth-in-this-route"],
+  publicRoutes: ["/api/webhooks/clerk"],
 });
 
 export const config = {
